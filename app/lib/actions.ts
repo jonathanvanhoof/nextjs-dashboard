7c3b48bd9ec9828f207f483d607fc409b5803223
jonathanvanhoof <jonathan.vanhoof@easyfairs.com>
2025-10-17 10:55:00 +0200

    nextjs dashboard chapter 12 (create invoice)

---
+++ app/lib/actions.ts
@@ -0,0 +1,40 @@
+'use server';
+
+import { z } from 'zod';
+import postgres from 'postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData): Promise<void> {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  // Next.js has a client-side router cache that stores the route segments in the user's browser for a time.
+  // Along with prefetching, this cache ensures that users can quickly navigate between routes while reducing the number of requests made to the server.
+  // Since we're updating the data displayed in the invoices route, we want to clear this cache and trigger a new request to the server.
+  // We can do this with the revalidatePath function from Next.js
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
